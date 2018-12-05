8ef5f6d84e72ef42ad46f86956381ef0f79bd292
Aiko Ramalho <aiko.ramalho@fu2re.com.br>
2018-12-05 21:45:55 -0200

    adiciona victory

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SimpleAppBar from './Appbar'
+import * as V from 'victory';
+import { VictoryChart, VictoryLine, VictoryTheme } from 'victory';
 
 class App extends Component {
+  
   render() {
+    const data = [
+      {x: 1, y: 13000},
+      {x: 2, y: 16500},
+      {x: 3, y: 14250},
+      {x: 4, y: 19000}
+    ];
     return (
       <div class="wrapper">
         <header class="header">
@@ -12,6 +21,11 @@ class App extends Component {
         </header>
         <article class="main">
           <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>  
+          <VictoryChart
+            theme={VictoryTheme.material}
+          >
+            <VictoryLine data={data}/>
+          </VictoryChart>
         </article>
         <footer class="footer">Footer</footer>
       </div>
